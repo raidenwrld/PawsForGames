44248a6a775afa7866749b38a2586cd9d358c94c
raidenwrld <150138064+raidenwrld@users.noreply.github.com>
2025-05-11 12:01:01 +0100

    disappearing platform
    prefabs made

---
+++ Assets/Scripts/DisappearingPlat.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlat : MonoBehaviour
+{
+    public string playerTag = "Player";
+
+    private float timer = 0f;
+    private bool playerOnTop = false;
+
+    void Update()
+    {
+        if (playerOnTop)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 2f)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            timer = 0f; // reset timer if player steps off
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(playerTag))
+        {
+            playerOnTop = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag(playerTag))
+        {
+            playerOnTop = false;
+        }
+    }
+}
